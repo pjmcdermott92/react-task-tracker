203c5b59f2cd3b88884043a022b4f6af45b58273
Patrick <patrick@pjmcdermott.com>
2021-02-21 23:39:36 -0700

    commit finished project

---
+++ src/components/Button.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+
+function Button({ color, text, onClick }) {
+    return (
+        <button onClick={onClick} className='btn' style={{backgroundColor: color}}>{text}</button>
+    )
+}
+
+Button.defaultProps = {
+    color: 'steeleblue'
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func,
+}
+
+export default Button
